4619eefacdd80c6ef4d8aaaddbe44820682d0386
Niall Thurrat <nt222fc@student.lnu.se>
2020-02-29 18:13:29 +0100

    connect to mongoDB atlas created

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
+/**
+ * The starting point of the application.
+ *
+ * @author Niall Thurrat
+ * @version 1.0.0
+ */
 
-var express = require('express')
-var app = express()
-var port = process.env.PORT || 3000
+'use strict'
 
-app.listen(port)
+const express = require('express')
+const mongoose = require('./config/mongoose')
 
-console.log('fish list RESTful API server started on: ' + port)
+const app = express()
+
+// connect to mongoDB via mongoose
+mongoose.run().catch(error => {
+  console.error(error)
+  process.exit(1)
+})
+
+app.listen(3000, () => console.log('Server running at http://localhost:3000/'))
