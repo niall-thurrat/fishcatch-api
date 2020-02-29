89399bee5c06362058dcd1b7bcd41bd0b35c7b15
Niall Thurrat <nt222fc@student.lnu.se>
2020-02-29 18:41:35 +0100

    fishCatch model created

---
+++ models/fishCatchModel.js
@@ -0,0 +1,45 @@
+/**
+ * Mongoose model of a fish catch.
+ *
+ * @author Niall Thurrat
+ * @version 1.0.0
+ */
+
+'use strict'
+
+const mongoose = require('mongoose')
+
+// Create a fish catch schema.
+const fishCatchSchema = new mongoose.Schema({
+  catcherName: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  catchLatitude: {
+    type: Number,
+    required: true
+  },
+  catchLongitude: {
+    type: Number,
+    required: true
+  },
+  species: {
+    type: String,
+    required: true
+  },
+  weight: {
+    type: Number,
+    required: true
+  },
+  length: {
+    type: Number,
+    required: true
+  }
+}, { timestamps: true })
+
+// Create a model using the schema.
+const FishCatch = mongoose.model('fishCatch', fishCatchSchema)
+
+// Exports.
+module.exports = FishCatch
