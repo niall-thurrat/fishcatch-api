ec20525666d2d5675685292e8cf644fbb9af62fa
Niall Thurrat <nt222fc@student.lnu.se>
2020-02-29 17:57:24 +0100

    empty routes, models and controllers added, nodemon added and app.js connects to port 3000

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-console.log('hellllo!')
+
+var express = require('express')
+var app = express()
+var port = process.env.PORT || 3000
+
+app.listen(port)
+
+console.log('fish list RESTful API server started on: ' + port)
