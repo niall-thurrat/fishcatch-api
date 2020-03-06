699078abac6b2feae4b46474ab29182573fda4e8
Niall Thurrat <nt222fc@student.lnu.se>
2020-03-06 12:49:34 +0100

    hash, jwt and credential check methods added

--- models/userModel.js
+++ models/userModel.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const userSchema = mongoose.Schema({
   name: {
@@ -11,8 +13,49 @@ const userSchema = mongoose.Schema({
     type: String,
     required: true,
     trim: false
-  }
+  },
+  tokens: [{
+    token: {
+      type: String,
+      required: true
+    }
+  }]
 }, { timestamps: true })
+
+// Hash password before saving user model
+userSchema.pre('save', async function (next) {
+  const user = this
+  if (user.isModified('password')) {
+    user.password = await bcrypt.hash(user.password, 8)
+  }
+
+  next()
+})
+
+// Generate auth token for user
+userSchema.methods.generateAuthToken = async function () {
+  const user = this
+  const token = jwt.sign({ _id: user._id }, process.env.JWT_KEY)
+  user.tokens = user.tokens.concat({ token })
+  await user.save()
+
+  return token
+}
+
+// Search for user by name and password.
+userSchema.statics.findByCredentials = async (name, password) => {
+  const user = await User.findOne({ name })
+  if (!user) {
+    throw new Error({ error: 'Invalid login credentials' })
+  }
+
+  const isPasswordMatch = await bcrypt.compare(password, user.password)
+  if (!isPasswordMatch) {
+    throw new Error({ error: 'Invalid login credentials' })
+  }
+
+  return user
+}
 
 const User = mongoose.model('User', userSchema)
 
