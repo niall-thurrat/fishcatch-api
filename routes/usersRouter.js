f9cb810914532a6b419fb0282e79a1c6973707e6
Niall Thurrat <nt222fc@student.lnu.se>
2020-03-10 10:17:49 +0100

    authz middleware route removed

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -9,12 +9,11 @@
 
 const router = require('express').Router()
 const controller = require('../controllers/usersController')
-const authz = require('../middleware/authz')
 
 router.post('/signup', controller.signup)
 router.post('/login', controller.login)
-router.get('/:username', authz, controller.viewUser)
-router.post('/:username/logout', authz, controller.logoutUser)
+router.get('/:username', controller.viewUser)
+router.post('/:username/logout', controller.logoutUser)
 
 // Exports.
 module.exports = router
