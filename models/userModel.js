24f1cfadd09a009f095aac92d08fa05e81cab01f
Niall Thurrat <nt222fc@student.lnu.se>
2020-03-10 13:04:31 +0100

    password hash and compare methods added to model

--- models/userModel.js
+++ models/userModel.js
@@ -9,6 +9,7 @@
 'use strict'
 
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
 
 const userSchema = mongoose.Schema({
   name: String,
@@ -30,6 +31,21 @@ const userSchema = mongoose.Schema({
     trim: false
   }
 }, { timestamps: true })
+
+// using pre-hook to hash password
+userSchema.pre('save', async function (next) {
+  const user = this
+  const hash = await bcrypt.hash(user.password, 10)
+  user.password = hash
+  next()
+})
+
+// Compare hashed login password with database password
+userSchema.methods.isValidPassword = async function (password) {
+  const user = this
+  const compare = await bcrypt.compare(password, user.password)
+  return compare
+}
 
 const User = mongoose.model('User', userSchema)
 
