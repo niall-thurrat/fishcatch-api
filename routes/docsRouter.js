c9f4ef24c56be83f61f23048935b910c424033c2
Niall Thurrat <nt222fc@student.lnu.se>
2020-03-17 23:27:57 +0100

    routes fixed

--- routes/docsRouter.js
+++ routes/docsRouter.js
@@ -14,8 +14,8 @@ router.get('/rels/signup', controller.signupDoc)
   .get('/rels/login', controller.loginDoc)
   .get('/rels/user', controller.userDoc)
   .get('/rels/user-fish', controller.userFishDoc)
-  // .get('/rels/all-fish', controller.userFishDoc)
-  // .get('/rels/one-fish', controller.userFishDoc)
+  // .get('/rels/all-fish', controller.allFishDoc)
+  // .get('/rels/one-fish', controller.oneFishDoc)
 
 // Exports
 module.exports = router
