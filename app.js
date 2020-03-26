e6d0bfea19e4a2c218d067565f70764c3177dc30
Niall Thurrat <nt222fc@student.lnu.se>
2020-03-26 17:07:54 +0100

    http-errors installed, custom error handler added

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ const express = require('express')
 const mongoose = require('./config/mongoose')
 const passport = require('passport')
 const bodyParser = require('body-parser')
+const createError = require('http-errors')
 const logger = require('morgan')
 
 const app = express()
@@ -35,6 +36,19 @@ app.use('/users', require('./routes/usersRouter'))
 app.use('/fish', passport.authenticate(
   'jwt', { session: false }), require('./routes/fishCatchRouter'))
 app.use('/docs', require('./routes/docsRouter'))
+
+// catch 404 errors
+app.use('*', (req, res, next) => next(createError(404)))
+
+// custom error handler
+app.use((error, req, res, next) => {
+  res.status(error.status || 500)
+  res.json({
+    status: error.status,
+    message: error.message,
+    stack: error.stack
+  })
+})
 
 // run server
 app.listen(port, () => console.log(`Server running on port ${port}`))
