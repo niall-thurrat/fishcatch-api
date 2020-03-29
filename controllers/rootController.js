6704ac1b95b9200d5466e3018b0ce7c360e087ed
Niall Thurrat <nt222fc@student.lnu.se>
2020-03-29 18:54:18 +0200

    refactoring

--- controllers/rootController.js
+++ controllers/rootController.js
@@ -11,38 +11,60 @@ const halson = require('halson')
 
 const rootController = {}
 
-rootController.getRoot = async (req, res, next) => {
+/**
+ * API root
+ * Handling GET requests to / endpoint
+ *
+ * @param {Object} req - request object
+ * @param {Object} res - response object
+ * @param {Function} next - next middleware func
+ *
+ */
+rootController.getRoot = (req, res, next) => {
   try {
-    const resource = halson({
-      meta: {
-        title: 'FishCatch RESTful API',
-        author: 'Niall Thurrat',
-        description: 'The FishCatch RESTful API serves up user and fish resources' -
-        'as well as accompanying API docs for client developers'
-      },
-      description: 'This is the root/main entry point of the API. Login requires ' +
-        'signup first. Created user recources require jwt token from login request.'
-    }).addLink('self', '/')
-      .addLink('curies', [{
-        name: 'fc',
-        href: `https://${req.headers.host}/docs/rels/{rel}`,
-        templated: true
-      }])
-      .addLink('fc:signup', '/users/signup')
-      .addLink('fc:login', '/users/login')
-      .addLink('fc:user', {
-        href: '/users/{username}',
-        templated: true
-      })
-      .addLink('fc:fish', '/fish')
-
     res.status(200)
     res.setHeader('Content-Type', 'application/hal+json')
 
-    res.send(JSON.stringify(resource))
+    const resBody = setResBody(req, res)
+
+    res.send(JSON.stringify(resBody))
   } catch (error) {
     next(error)
   }
+}
+
+/**
+ * Returns a HAL formatted JSON object
+ *
+ * @param {Object} request
+ * @param {Object} response
+ *
+ */
+function setResBody (req, res) {
+  const resBody = halson({
+    meta: {
+      title: 'FishCatch RESTful API',
+      author: 'Niall Thurrat',
+      description: 'The FishCatch RESTful API serves up user and fish resources ' -
+      'as well as accompanying API docs for client developers'
+    },
+    description: 'This is the root/main entry point of the API. Signup required ' +
+      'then jwt token from login before access to any other resource.'
+  }).addLink('self', '/')
+    .addLink('curies', [{
+      name: 'fc',
+      href: `https://${req.headers.host}/docs/rels/{rel}`,
+      templated: true
+    }])
+    .addLink('fc:signup', '/users/signup')
+    .addLink('fc:login', '/users/login')
+    .addLink('fc:user', {
+      href: '/users/{username}',
+      templated: true
+    })
+    .addLink('fc:fish', '/fish')
+
+  return resBody
 }
 
 // Exports.
